ffb931a921963d6583bb50a2bc9300306db1c309
JaredP94 <jaredping@yahoo.com>
2017-08-16 00:21:36 +0200

    Exercise 5.1

--- stopwatch/StopWatch.cpp
+++ stopwatch/StopWatch.cpp
@@ -1,10 +1,23 @@
 #include "StopWatch.h"
-#include <ctime>
-using namespace std;
+
+// construct stopwatch object
+stopwatch::stopwatch(){}
 
 // returns the amount of time in seconds that has passed since the process (i.e. your program) started executing 
-double getProcessTime()
+double stopwatch::getProcessTime()
 {
-	clock_t time = clock();
+	time = clock();
 	return static_cast<double>(time)/CLOCKS_PER_SEC;
+}
+
+// sets starting point for timer
+void stopwatch::start()
+{
+	total = getProcessTime();
+}
+
+// stops timer and displays result
+double stopwatch::stop()
+{
+	return getProcessTime() - total;
 }
